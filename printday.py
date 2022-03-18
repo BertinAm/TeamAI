d1c9a3e80c1e4434e2f5dba57e2969cd66ac64c7
Ndalegh Noela <88977319+ndaleghnoelalummbah@users.noreply.github.com>
2022-03-18 08:08:32 +0100

    Create printday.py

---
+++ printday.py
@@ -0,0 +1,9 @@
+#18/03/22
+#Noela completed printday.py
+day={0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday"}
+def printday(number):
+    print("the day is",day[number])
+printday(int(input("enter the number")))
+
+
+
