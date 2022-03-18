c15b0beec8c5d18dcff2781cd120394f10cc66ca
Ndalegh Noela <88977319+ndaleghnoelalummbah@users.noreply.github.com>
2022-03-18 07:45:20 +0100

    Create hypothenuse.py

---
+++ hypothenuse.py
@@ -0,0 +1,8 @@
+#18/03/22
+#Noela completed hypothenuse.py
+import math
+print("enter the lenght of the 2 sides")
+def hypothenuse(a,b):
+    c=math.sqrt((a*a)+(b*b))
+    print("the hypothenuse off this right_angled triangle is",c)
+hypothenuse(int(input("enter side a")),int(input("enter side b")))3
