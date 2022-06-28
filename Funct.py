bf5abae51fc265deaff90ddda2f19c64ccbdb526
ndaleghnoelalummbah <ndaleghnoela@gmail.com>
2022-06-28 02:57:02 +0000

    Functs #47

---
+++ Funct.py
@@ -0,0 +1,7 @@
+# calling a function using a differentn function name
+def add(a,b):
+    return(a+b)
+def sum(x):
+    return(x)
+x=add(int(input("enter a ")),int(input("enter b ")))
+print(sum(x))
